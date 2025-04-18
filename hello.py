8a81896d67b99a01883ff574c6180900e4e64a51
VanDrew01 <doylesean12@gmail.com>
2025-04-18 10:59:38 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-# Firstname Lastname
-# 11/15/2024
+# Andrew Van
+# 4/17/2025
 # A simple Python program to print a message
 
+# Andrew Van
+# 4/18/2025
+# added new message
+
 print("Hello, World!")
+
+print("Welcome to GitHub")
